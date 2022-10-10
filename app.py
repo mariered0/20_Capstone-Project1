1ba60a840b29eb3f6d801355265926eaaf3e4423
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-10-10 11:41:29 -0700

    update auth config vars

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 uri = os.environ.get("DATABASE_URL")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///booklyn_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
